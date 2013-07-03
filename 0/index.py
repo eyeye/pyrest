ce1339b331c3e0ea545db4f995517e21e1ad6688
YangZhiyong <yzy@autoemaster.com>
2013-07-03 16:59:25 +0800

    测试 flask restful

--- 0/index.py
+++ 0/index.py
@@ -1,11 +1,27 @@
 #-*- coding:utf-8 -*-
 
-def app(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    body=["Welcome to EYE Cloud! TEST PUSH TEST Pycharm\n"]
-    return body
+from flask import Flask, g, request
+import sys
+    sys.path.insert(0, APP_DIR/flask_restful)
+
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+app.debug = True
+
+api = Api(app)
+
+# @app.route('/')
+# def hello():
+#     return "Hello, world! - Flask\n"
+
+
+class TodoSimple(Resource):
+    def get(self):
+        return {"Hello": "World!"}
+
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 from bae.core.wsgi import WSGIApplication
 application = WSGIApplication(app)
