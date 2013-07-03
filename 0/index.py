236aa06db501c5c0651c36f37d53c7638f200a07
baeadmin <baeadmin@baidu.com>
2013-07-04 01:30:03 +0800

    commit by edit_adapter

--- 0/index.py
+++ 0/index.py
@@ -3,7 +3,11 @@ from bae.core import const
 from flask import Flask, g, request
 import sys
 
-sys.path.insert(0, const.APP_DIR)
+# sys.path.insert(0, const.APP_DIR)
+
+path = os.path.dirname(os.path.abspath(__file__)) + '/flask_restful'
+if path not in sys.path:
+  sys.path.insert(1, path)
 
 from flask_restful import Resource, Api
 
