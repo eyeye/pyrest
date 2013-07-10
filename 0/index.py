96c01b6dfd6e1efc7c73f30a958f05fc1a717670
YangZhiyong <yzy@autoemaster.com>
2013-07-10 17:47:27 +0800

    pymongo 和 rsa 测试。

--- 0/index.py
+++ 0/index.py
@@ -2,7 +2,8 @@
 
 import os
 import sys
-
+from Crypto.PublicKey import RSA
+import json
 
 if 'SERVER_SOFTWARE' in os.environ:
     from bae.core import const
@@ -19,13 +20,28 @@ path = os.path.dirname(os.path.abspath(__file__)) + '/flask_restful'
 if path not in sys.path:
     sys.path.insert(1, path)
 
-from flask_restful import Resource, Api
-
+from flask_restful import Resource, Api, reqparse, abort
+from flask_pymongo import PyMongo
+from bson import json_util
 
 app = Flask(__name__)
 app.debug = True
 api = Api(app)
 
+
+################## Mongo ###########################################
+
+if 'SERVER_SOFTWARE' in os.environ:
+    app.config['MONGO_HOST'] = const.MONGO_HOST
+    app.config['MONGO_PORT'] = const.MONGO_PORT
+    app.config['MONGO_DBNAME'] = 'GeEBBeqAoMglOIEuqZaD'
+else:
+    app.config['MONGO_HOST'] = '127.0.0.1'
+    app.config['MONGO_PORT'] = 27017
+    app.config['MONGO_DBNAME'] = 'local'
+
+# app.config['MONGO_DBNAME'] = 'local'
+mongo = PyMongo(app, config_prefix='MONGO')
 
 #############################################################
 
@@ -50,6 +66,85 @@ class HelloWho(Resource):
 
 api.add_resource(HelloWho, '/<string:who>')
 
+
+Todos = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+}
+
+parser = reqparse.RequestParser()
+parser.add_argument('task', type=str)
+
+
+def abort_no_exist(todo_id):
+    if todo_id not in Todos:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+class Todo(Resource):
+    def get(self, todo_id):
+        abort_no_exist(todo_id)
+        return {todo_id: Todos[todo_id]}
+
+    def put(self, todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        Todos[todo_id] = task
+        return task, 201
+
+    def delete(self, todo_id):
+        abort_no_exist(todo_id)
+        del Todos[todo_id]
+        return '', 204
+
+api.add_resource(Todo, '/<string:todo_id>')
+
+
+
+class TodoList(Resource):
+    def get(self):
+        return Todos
+
+api.add_resource(TodoList, '/todolist')
+
+
+class DBTest(Resource):
+    def get(self):
+
+        print 'findone'
+        todo = mongo.db.todos.find_one()
+        print json_util.dumps(todo)
+
+        print 'find'
+        list = mongo.db.todos.find()
+        # print json_util.dumps(list)
+        return json_util.dumps(list, default=json_util.default)
+        # print json_util.dumps({'a': 'b', 'c': 'd'})
+        # return json_util.dumps({'a': 'b', 'c': 'd'})
+
+    def put(self):
+        print 'put'
+
+api.add_resource(DBTest, '/mongotest')
+
+
+
+class RSA_Test(Resource):
+    def get(self):
+        print 'RSA_Test put'
+
+        key = RSA.generate(1024)
+        '========== pubkey ==================='
+        print key
+        print key.exportKey('PEM')
+
+        print '========== pubkey ==================='
+        pubkey = key.publickey()
+        print pubkey.exportKey('PEM')
+
+        return key.exportKey('PEM')
+
+api.add_resource(RSA_Test, '/rsatest')
 
 #############################################################
 
